bdfc14ceed249fb4b020bd6368f3868cd88f8972
nithin <nithin.yadla@bilvantis.io>
2025-04-07 12:23:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "bkt-demo-000"
+  name          = "wif-nithin-234"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "bilvantisaimlproject"
   force_destroy = true
   public_access_prevention = "enforced"
 }
 
 resource "google_storage_bucket" "my-bucket2" {
-  name          = "bkt-demo-002"
+  name          = "wif-nithin-235"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "bilvantisaimlproject"
   force_destroy = true
   public_access_prevention = "enforced"
 }
